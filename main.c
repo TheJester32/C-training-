765fa5eae31e525a1ac60f74e3d6e4aecc660200
TheJester32 <bogdanpopoveducation@gmail.com>
2023-08-21 19:15:16 +0300

    first steps!
    first steps!

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+//Ctrl + Alt + N
 
-int main(void)
-{
-    printf("Hello, world!");
-    return 0;
+// int main(void)
+// {
+//     int x = 3, y = -4;
+// char ch_eq = '=';
+// char ch_plus = '+'; 
+// char ch_minus = '-'; 
+// char ch_div = '/'; 
+// char ch_mul = '*';
+// double res = -0.75;  
+
+
+// printf("%d %c %d = ?\n", x, ch_div, y );
+// }
+
+#include <stdio.h>
+
+int main() {
+    char ch_n = 'N';
+    char ch_w = 'W';
+    char ch_dot = '.';
+    char ch_br = '"';
+    int x = 51;
+    int y = 0;
+    int a = 31;
+    int b = 25;
+    int c = 48;
+    int d = 9;
+    int e = 29;
+    int f = 93;
+    
+    printf("S.Holmes:\n%dgrad %d\'%d%c%d%c %c\n%d grad  %d\'%d%c%d%c %c",x,a,b,ch_dot,c,ch_br,ch_n,y,d,e,ch_dot,f,ch_br,ch_w);
+  return 0;
 }
